6aa22e13eba7e94aa4d895e57f4719a77a285e8c
nrlizza <nurul.izza@students.paramadina.ac.id>
2026-05-17 21:02:56 +0700

    [UPDATE] server.ts for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,26 @@
 import "dotenv/config";
 import app from "./app";
 
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const isProduction = process.env.NODE_ENV === "production";
 
-app.listen(port, () => {
-    console.log(`🚀 Server running on http://localhost:${port}`);
-});
+/* -------------------------------------------------------------------------- */
+/*                           Development Environment                          */
+/* -------------------------------------------------------------------------- */
+
+if (!isProduction) {
+    const port = process.env.PORT
+        ? Number(process.env.PORT)
+        : 3000;
+
+    app.listen(port);
+
+    console.log(
+        `🚀 Server running at http://localhost:${port}`
+    );
+}
+
+/* -------------------------------------------------------------------------- */
+/*                           Production Environment                           */
+/* -------------------------------------------------------------------------- */
+
+export default app;
