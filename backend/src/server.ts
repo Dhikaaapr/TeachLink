2af04c519925eb919d8c566b56cfe21248558fc3
nrlizza <nurul.izza@students.paramadina.ac.id>
2026-05-17 21:18:55 +0700

    update docker file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,26 +1,8 @@
 import "dotenv/config";
 import app from "./app";
 
-const isProduction = process.env.NODE_ENV === "production";
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-/* -------------------------------------------------------------------------- */
-/*                           Development Environment                          */
-/* -------------------------------------------------------------------------- */
-
-if (!isProduction) {
-    const port = process.env.PORT
-        ? Number(process.env.PORT)
-        : 3000;
-
-    app.listen(port);
-
-    console.log(
-        `🚀 Server running at http://localhost:${port}`
-    );
-}
-
-/* -------------------------------------------------------------------------- */
-/*                           Production Environment                           */
-/* -------------------------------------------------------------------------- */
-
-export default app;
+app.listen(port, () => {
+    console.log(`🚀 Server running on http://localhost:${port}`);
+});
